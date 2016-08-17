76a221a9ea44077a1c06bf79cb0ba86f7867a33c
Bo HOU <daily.bhou@gmail.com>
2016-08-17 23:18:10 +0200

    update version

--- examples/login/js/login.js
+++ examples/login/js/login.js
@@ -9,7 +9,7 @@ function getParameterByName(name, url) {
 }
 
 if (getParameterByName("dev")) {
-  collar.enableDevtool();
+  collar.enableDevtool({url : "http://localhost:8000"});
 }
 
 // create 'login' namespace
