9fab0525965cf9e1b715e71201eebf219abe80fa
Bo HOU <daily.bhou@gmail.com>
2016-08-25 11:53:38 +0200

    simplify the login example

---
+++ examples/login/js/login_basic.js
@@ -0,0 +1,55 @@
+collar.enableDevtool();
+
+var ns = collar.ns('com.collarjs.example.login');
+var loginViewSensor = ns.sensor('ui sensor', function(options) {
+  $( "#signin-btn" ).click(() => {
+    this.send({
+      event : 'signin'
+    });
+  });
+});
+
+var loginPipeline = loginViewSensor
+  .when('user click signin button',
+    {event : "must be 'signin'"}, signal => {
+    return signal.get('event') === 'signin';
+  })
+  .do('get user credential from view',
+    {},
+    {__result__ : "object contains 'email' and 'password'"}, signal => {
+    var email = $( "#emailInput" ).val();
+    var password = $( "#passwordInput" ).val();
+
+    return {
+      email : email,
+      password : password
+    }
+  })
+  .do('check user credential',
+    {"__result__.email": "user email", "__result__.password": "user password"},
+    {__result__: "ok or failed reason"}, signal => {
+    var email = signal.getResult().email;
+    var password = signal.getResult().password;
+
+    if (email === "test@collarjs.com" && password === "collarjs") {
+      return "ok";
+    } else {
+      return "email and password not match";
+    }
+  })
+
+loginPipeline
+  .when('credential ok', {__result__ : "must be 'ok'"}, signal => {
+    return signal.getResult() === "ok";
+  })
+  .do('alert login ok', signal => {
+    alert("login ok!");
+  });
+
+loginPipeline
+  .when('credential not ok', {__result__ : "must not be 'ok'"}, signal => {
+    return signal.getResult() !== "ok";
+  })
+  .do('alert login failed', signal => {
+    alert("login failed!");
+  })
